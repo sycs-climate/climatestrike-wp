6928782ca647a05d22c9325467e4c449fce908ed
Emil Carr <emil.carr@protonmail.com>
2020-01-19 02:08:13 +0000

    Catch and display ecr_PostcodeNotFoundException added in v1.01

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 
 This is the wordpress theme and plugin powering https://climatestrike,scot , the website of the Scottish Youth Climate Strike.
 
-Requires ECR's Things plugin: https://github.com/FalseAscension/ECRs-Things
+Requires ECR's Things v1.01 or greater: https://github.com/FalseAscension/ECRs-Things
--- themes/climatestrike/page-strike.php
+++ themes/climatestrike/page-strike.php
@@ -11,8 +11,14 @@ $eventsQuery = $eventsObj->buildQuery('', '', 'strike');
 $events = $eventsObj->getEvents($eventsQuery);
 
 if(isset($_GET['near'])) {
-    $eventsObj->addDistance($events, $_GET['near']);
-    usort($events, function($a, $b){ return $a->distance - $b->distance; }); // Assume we want to sort by closest
+    try {
+        $eventsObj->addDistance($events, $_GET['near']);
+        usort($events, function($a, $b){ return $a->distance - $b->distance; }); // Assume we want to sort by closest
+    } catch(ecr_PostcodeNotFoundException $e) {
+        $context['error'] = "Postcode not found. Please enter a valid postcode.";
+    } catch(Exception $e) {
+        $context['error'] = "Something went wrong, sorry.";
+    }
 }
 
 $context['events'] = $events;
